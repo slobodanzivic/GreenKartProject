031f2fd0904315de730a488af45f06d6b48b49c4
slobodan <slobodan.m.zivic@gmail.com>
2020-10-08 01:04:30 +0200

    Adding beforeEach method

--- cypress/integration/examples/GreenKartTest.js
+++ cypress/integration/examples/GreenKartTest.js
@@ -5,13 +5,20 @@ describe('GreenKart test', function(){
 
     const homePage = new HomePage()
 
-    it('Go to https://rahulshettyacademy.com/seleniumPractise/#/',function(){
+    beforeEach(() =>{
         cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
+    })
+
+    it('Go to https://rahulshettyacademy.com/seleniumPractise/#/',function(){
         cy.url().should('equal', 'https://rahulshettyacademy.com/seleniumPractise/#/')
     })
 
     it('Validate Page Title', function(){
-        cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
         cy.title().should('equal', 'GreenKart - veg and fruits kart')
+    })
+
+    it('Check how many products are there in the page', function(){
+        cy.get('h4.product-name').should('have.length', '30')
+
     })
 })
