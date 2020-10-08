55c6f92ced898972f5afe21622468359b6db1219
slobodan <slobodan.m.zivic@gmail.com>
2020-10-08 18:24:53 +0200

    Adding test and two functions

--- cypress/integration/examples/GreenKartTest.js
+++ cypress/integration/examples/GreenKartTest.js
@@ -1,25 +1,31 @@
 ///<reference types="Cypress"/>
 import HomePage from '../../support/pageObjects/HomePage.js'
 
-describe('GreenKart test', function(){
+describe('GreenKart test', function () {
 
     const homePage = new HomePage()
 
-    beforeEach(() =>{
+    beforeEach(() => {
         cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
     })
 
-    it('Go to https://rahulshettyacademy.com/seleniumPractise/#/',function(){
+    it('Go to https://rahulshettyacademy.com/seleniumPractise/#/', function () {
         cy.url().should('equal', 'https://rahulshettyacademy.com/seleniumPractise/#/')
     })
 
-    it('Validate Page Title', function(){
+    it('Validate Page Title', function () {
         cy.title().should('equal', 'GreenKart - veg and fruits kart')
     })
 
-    it('Check how many products are there in the page', function(){
+    it('Check how many products are there in the page', function () {
         cy.get('h4.product-name').should('have.length', '30')
-        
 
+    })
+
+    it('Type "ca" in search field, add Carrot to cart and validate whether Carrot is in the cart', function () {
+        homePage.clickOnSearchField().type('ca').should('have.value', 'ca')
+        homePage.chooseProductAndclickOnAddToCartBtn()
+        homePage.clickOnCart()
+        homePage.validateProductInTheCart()
     })
 })
--- cypress/support/pageObjects/HomePage.js
+++ cypress/support/pageObjects/HomePage.js
@@ -1,15 +1,35 @@
-class HomePage{
+class HomePage {
 
-    clickOnSearchField () {
+    clickOnSearchField() {
         return cy.get('input[type=search]').click()
     }
 
-    clickOnSearchButton () {
+    clickOnSearchButton() {
         return cy.get('button.search-button').click()
     }
 
-    clickOnCart () {
-        return cy.get('a.cart-icon img')
+    clickOnCart() {
+        return cy.get('a.cart-icon img').click()
+    }
+
+    chooseProductAndclickOnAddToCartBtn() {
+        cy.get('h4.product-name').each(($el, index, $list) => {
+            const productName = $el.text()
+            if (productName.includes("Carrot")) {
+                cy.get('div.product-action button').eq(index).click()
+            }
+
+        })
+    }
+
+    validateProductInTheCart() {
+        cy.get('li.cart-item div[class="product-info"] p').each(($el, index, $list) => {
+            const productName = $el.text()
+            if (productName.includes("Carrot")) {
+                expect(true).to.be.true
+            }
+
+        })
     }
 
 }
